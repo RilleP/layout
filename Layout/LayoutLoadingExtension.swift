07557ad9a9217ac90c305b29cdbe60d9fcaaaadf
Rikard <rikkypikki@hotmail.com>
2018-08-01 11:34:45 +0200

    stuff

---
+++ Layout/LayoutLoadingExtension.swift
@@ -0,0 +1,9 @@
+//
+//  LayoutLoadingExtension.swift
+//  Skinsight
+//
+//  Created by Rikard Petre on 2018-01-24.
+//  Copyright © 2018 Moose Medical. All rights reserved.
+//
+
+import Foundation
